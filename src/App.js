959a4ac19b61193a5773b5b220ed9daccd7ad542
Jonatan Musciachio <jonamusciachio@gmail.com>
2022-04-20 16:47:11 -0300

    Entrega clase 9

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import NavBar from '././Components/NavBar/NavBar';
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
-import ItemCount from './Components/ItemCount/ItemCount';
 import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Alert from '@mui/material/Alert';
-import { useState } from 'react';
+import { CartContextProvider } from './Context/CartContext'
+
+
 
 function App() {
-  const [cart, setCart] = useState([])
-  console.log(cart);
   return (
     <div className="App">
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path='/' element={<ItemListContainer />} />
-          <Route path='/category/:categoryId' element={<ItemListContainer />} />
-          <Route path='/detail/:productId' element={<ItemDetailContainer setCart={setCart} cart={cart}/>} />
-          <Route path='*' element={<Alert variant="filled" severity="error">NOT FOUND 404</Alert>}/>
-        </Routes>
-      </BrowserRouter>
+      <CartContextProvider>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<ItemListContainer />} />
+            <Route path='/category/:categoryId' element={<ItemListContainer />} />
+            <Route path='/detail/:productId' element={<ItemDetailContainer />} />
+            <Route path='*' element={<Alert variant="filled" severity="error">NOT FOUND 404</Alert>} />
+          </Routes>
+        </BrowserRouter>
+        </CartContextProvider>
     </div>
   );
 }
